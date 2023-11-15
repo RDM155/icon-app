46f91ac2ba86e2ae9c389a66df9e9a39a8471efb
Ricardo Diaz <ricardo.diaz@grupoconsultic.com.mx>
2023-11-15 16:32:07 -0600

    All completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { BiAdjust } from "react-icons/bi";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
 
 function App() {
+  const clientID =
+    "1088649719161-st10u6c0upj4oqak38f9do4a6b9vmavh.apps.googleusercontent.com";
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const start = () => {
+      gapi.auth2.init({
+        clientId: clientID,
+      });
+    };
+    gapi.load("client:auth2", start);
+  }, []);
+
+  const onSuccess = (response) => {
+    setUser(response.profileObj);
+  };
+
+  const onFailure = () => {
+    console.log("F Something went wrong");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId={clientID}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_policy"}
+        render={(renderProps) => (
+          <div className="button-container">
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <AiFillGoogleCircle />
+            </button>
+          </div>
+        )}
+      />
+      <br/><br/><br/><br/><br/>
+      <div className={user ? "profile" : "hidden"}>
+        <img src={user.imageUrl} alt="" />
+        <h4>{user.name}</h4>
+      </div>
     </div>
   );
 }
